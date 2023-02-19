9fa88a942c3d079bfb4c35a291f8b0fc2057f060
athenaannk <121466685+athenaannk@users.noreply.github.com>
2023-02-19 11:56:54 -0500

    Update prework.py

--- prework.py
+++ prework.py
@@ -10,9 +10,46 @@ hello_username(name)
 # Question 2. Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing.
 
 def first_odds():
-    for num in range (1, 100):
-        if num % 2 != 0:
+    current_number = 0
+    while current_number < 100:
+        current_number += 1
+        if current_number % 2 == 0:
+            continue
+        print(current_number)
 
-            return num
+
+# Question 3. Please write a Python function, max_num_in_list to return the max number of a given list.
+
+def max_num_in_list (a_list):
+    max = a_list[0]
+    
+    for x in a_list:
+        if x > max:
+            max = x
+    return max
+
+a_list = [1, 19,55,65,89,302]
+print("The largest number in the list is: " + str(max_num_in_list(a_list)) + ".")
+
+
+# Question 4. Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+
+def is_leap_year(a_year):
+    leap = False
+    if a_year % 400 == 0:
+        leap = True
+    elif a_year % 4 == 0 and a_year % 100:
+        leap = True
+    return leap
+
+year = int(input())
+print(is_leap_year(year))
+
+# Question 5. Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+
+def is_consecutive(a_list):
+    return sorted(a_list) ==
+
+# really stuck on this one!
 
 
