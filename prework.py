e16e991c815be27f241490bb4e68013376ccecd6
athenaannk <121466685+athenaannk@users.noreply.github.com>
2023-02-19 16:21:52 -0500

    Update prework.py
    answered no. 2

--- prework.py
+++ prework.py
@@ -10,13 +10,10 @@ hello_username(name)
 # Question 2. Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing.
 
 def first_odds():
-    current_number = 0
-    while current_number < 100:
-        current_number += 1
-        if current_number % 2 == 0:
-            continue
-        print(current_number)
-
+    print("Odd numbers from 1-100 are: ")
+    for x in range(1,100 +1, 2):
+            print(x)
+print(first_odds())
 
 # Question 3. Please write a Python function, max_num_in_list to return the max number of a given list.
 
@@ -46,10 +43,7 @@ year = int(input())
 print(is_leap_year(year))
 
 # Question 5. Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
-
-def is_consecutive(a_list):
-    return sorted(a_list) ==
-
 # really stuck on this one!
+
 
 
